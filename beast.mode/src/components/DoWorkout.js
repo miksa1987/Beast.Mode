a487652ae09c524d2d1f7ee6cef8a435ba414fb3
miksa1987 <mika87@gmail.com>
2019-07-04 09:47:58 +0300

    Do workoutin hahmotusta

--- beast.mode/src/components/DoWorkout.js
+++ beast.mode/src/components/DoWorkout.js
@@ -1,19 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import communicationService from '../service/communication'
+import parser from '../service/parser'
 
 const DoWorkout = (props) => {
   const [workout, setWorkout] = useState({})
 
+  const style = { 
+    whiteSpace: 'pre-line'
+  }
+
+  const doWorkoutType0 = (exercises) => {
+
+  }
+
+  const doWorkoutType1 = (exercises) => {
+
+  }
+
   useEffect(() => {
+    // Gotta try to figure out cleaner way to do this.
     const fetchData = async () => {
       setWorkout(await communicationService.get(`/workouts/${props.workoutid}`))
     }
     fetchData()
   }, [])
+
+  useEffect(() => {
+    if (workout.content) {
+      let exercises = []
+      const lines = workout.content.split('\n')
+
+      lines.forEach(line => {
+        if (parser.isWorkout(line)) exercises.push(line)
+      })
+
+      if (exercises.length > 0) {
+        if (parser.match0(exercises[0])) doWorkoutType0(exercises)
+        if (parser.match1(exercises[0])) doWorkoutType1(exercises)
+      }
+    }
+  }, [workout])
   
-  return ( <div>
+  return ( <div style={style}>
     {workout.content}
-    <h3>TOIMMIII</h3>
   </div> )
 }
 
--- beast.mode/src/service/parser.js
+++ beast.mode/src/service/parser.js
@@ -2,9 +2,21 @@ const regex0 = /\d\d?x\d\d?\s+\D+/
 const regex1 = /\d\d?\s+\D+/
 
 const isWorkout = (post) => {
-  if(regex0.exec(post) || regex1.exec(post)) {
+  if (regex0.exec(post) || regex1.exec(post)) {
     return true
   }
+
+  return false
+}
+
+const match0 = (text) => {
+  if (regex0.exec(text)) return true
+
+  return false
+}
+
+const match1 = (text) => {
+  if (regex1.exec(text)) return true
 
   return false
 }
@@ -13,7 +25,7 @@ const getExercisesFrom = (post) => {
   const lines = post.split('\n')
   let exercises = []
   lines.forEach(line => {
-    if(regex0.exec(line) || regex1.exec(line)) {
+    if (regex0.exec(line) || regex1.exec(line)) {
       const exercise = line.substring(line.indexOf(' '))
       exercises = exercises.concat(exercise)
     }
@@ -22,4 +34,4 @@ const getExercisesFrom = (post) => {
   return exercises
 }
 
-export default { isWorkout, getExercisesFrom }
+export default { isWorkout, match0, match1, getExercisesFrom }
