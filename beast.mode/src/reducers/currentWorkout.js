f644dd76a9e44bad843d8c2d8de639fe76043970
miksa1987 <mika87@gmail.com>
2019-07-05 21:28:49 +0300

    Workoutin parsimisen toteutusta

---
+++ beast.mode/src/reducers/currentWorkout.js
@@ -0,0 +1,36 @@
+const emptyWorkout = {
+  type: null,
+  exercises: [],
+  time: {
+    time: 0,
+    visible: false
+  },
+  done: false
+}
+
+const currentWorkoutReducer = (state = emptyWorkout, action) => {
+  switch(action.type) {
+    case 'INIT_CURRENT_WORKOUT':
+      return emptyWorkout
+    case 'SET_CURRENT_WORKOUT_EXERCISES':
+      return { ...state, exercises: action.data }
+    case 'SET_CURRENT_WORKOUT_TIME':
+      return { ...state, time: { time: action.data.time, visible: action.data.visible }}
+    default:
+      return state
+  }
+}
+
+export const initCurrentWorkout = () => {
+  return dispatch => dispatch({ type: 'INIT_CURRENT_WORKOUT' })
+}
+
+export const setCurrentWorkoutExercises = (exercises) => {
+  return dispatch => dispatch({ type: 'SET_CURRENT_WORKOUT_EXERCISES', data: exercises })
+}
+
+export const setCurrentWorkoutTime = (time, visible) => {
+  return dispatch => dispatch({ type: 'SET_CURRENT_WORKOUT_TIME', data: { time, visible } })
+}
+
+export default currentWorkoutReducer
--- beast.mode/src/service/parser.js
+++ beast.mode/src/service/parser.js
@@ -37,4 +37,87 @@ const getExercisesFrom = (post) => {
   return exercises
 }
 
-export default { isWorkout, match0, match1, matchRounds, getExercisesFrom }
+const parseWorkout = (post) => {
+  if (post !== '') {
+    let workout = { exercises: [], rounds: 0 }
+    const lines = post.split('\n')
+
+    lines.forEach(line => {
+      if (isWorkout(line) && !(line.includes('rounds'))) {
+        workout.exercises.push(line)
+        workout.rounds 
+      }
+    })
+
+    if (exercises.length > 0) {
+      console.log('workout parsing')
+      if (parser.match0(exercises[0])) {
+        console.log('type 0')
+        setParsedWorkout(doWorkoutType0(exercises))
+        console.log(parsedWorkout)
+      }
+      if (parser.match1(exercises[0])) {
+        console.log('type 1')
+        const rounds = lines.find(line => parser.matchRounds(line))
+        setParsedWorkout(doWorkoutType1(exercises, rounds))
+      }
+    }
+  }
+}
+
+// Example: 5 pullups. Returns null if fails.
+const getRepsAndExercise = (text) => {
+  if (!match1(text)) return null
+
+  const whereToSplit = text.indexOf(' ')
+  const reps = Number(text.substring(0, whereToSplit))
+
+  if (typeof reps !== 'number') return null
+
+  const exercise = text.substring((whereToSplit + 1), (text.length - whereToSplit))
+
+  return { exercise, reps, done: false }
+}
+
+// Example: 5x5 pullups. Returns null if fails.
+const getRepsSetsAndExercise = (text) => {
+  if (!match0(text)) return null
+  const whereToSplit = text.indexOf('x')
+  const sets = Number(text.substring(0, whereToSplit))
+  if (typeof sets !== 'number') return null
+  const repsExercise = text.substring((whereToSplit + 1), (text.length - whereToSplit))
+
+  let setsRepsExercises = []
+  for (let i = 0; i < sets; i++) setsRepsExercises.push(getRepsAndExercise(repsExercise))
+
+  console.log(setsRepsExercises)
+  return setsRepsExercises
+}
+
+const doWorkoutType0 = (exercises) => {
+  let readyWorkout = { type: '0', exercises: [] }
+
+  exercises.forEach(line => {
+    const splits = line.split('x')
+    let sets = []
+    if(splits.length === 2) {
+      for (let i = 0; i < Number(splits[0]); i++) {
+        sets.push(splits[1])
+      }
+    }
+    console.log(`sets ${sets}`)
+    sets = sets.map((set, i) => <Exercise key={i} exercise={set} />)
+    readyWorkout.exercises.push(sets)
+  })
+  console.log(readyWorkout)
+  return readyWorkout
+}
+
+// Thought this was going to be longer but....
+const doWorkoutType1 = (exercises, rounds) => {
+  let readyWorkout = { type: '1', exercises, rounds }
+  readyWorkout.exercises = readyWorkout.exercises.map((ex, i) => <OneSetExercise key={i} exercise={ex} />)
+  return readyWorkout
+}
+
+export default { isWorkout, match0, match1, matchRounds, getExercisesFrom, parseWorkout }
