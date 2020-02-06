665c9e480c60a4573a5c3d87bbdda85b0b959dba
Mika <mika87@gmail.com>
2020-02-06 13:57:03 +0200

    Simplify Workout list component

--- frontend/src/components/workout/Workout.js
+++ frontend/src/components/workout/Workout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { Button, Image } from 'semantic-ui-react'
 import styled from 'styled-components'
 import parser from '../../service/parser'
@@ -7,18 +7,24 @@ import Animation from '../Animation'
 
 const Base = styled.div`
   display: grid;
-  grid-template: 2rem 7.3rem / 100%;
+  grid-template: 2rem 9.3rem 2rem / 100%;
   background-color: #ffffff;
-  margin: 10px;
-  padding: 10px;
+  margin: 10px 5px;
   border: 1px solid #dddddd;
-  border-radius: 4px;
 `
 
 const Columns = styled.div`
   display: grid;
-  grid-template: 100% / 5rem 5rem 5rem 1fr 5rem;
+  padding: 10px;
+  grid-template: 100% / 5rem 5rem 5rem 1fr;
   grid-gap: 5px;
+`
+
+const ViewWorkoutLink = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.15);
 `
 
 const Workout = (props) => {
@@ -34,11 +40,6 @@ const Workout = (props) => {
       exercises.slice(0, 5) : exercises.slice(0, exercises.length/2+1)
       
     return [linesToReturn, exercises.length]
-  }
-
-  const moveToWorkout = () => {
-    props.history.push(`/doworkout/${props.workout._id}`)
-    window.scrollTo(0, 0)
   }
 
   const [exercises, exercisesNumber] = getExercises()
@@ -69,12 +70,14 @@ const Workout = (props) => {
           <div>
             {exercises.map((exercise, i) => <p key={i}>{exercise}</p>)}
           </div>
-
-          <Button data-testid='viewworkoutbutton' id='viewworkoutbutton' floated='right' 
-              onClick={moveToWorkout}>
-              {` View`}
-          </Button>
         </Columns>
+
+        <ViewWorkoutLink>
+          <Link to={`/doworkout/${props.workout._id}`}>
+            <strong>View this workout</strong>
+          </Link>
+        </ViewWorkoutLink>
+        
       </Base>
     </Animation>
   )
